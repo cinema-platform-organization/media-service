65efdb475bf5c028fec003bf907ed18c9570dbee
Ruslan Parkhomchuk <parkhomchuk.lol@gmail.com>
2026-09-15 17:00:46 +0300

    Add delete functionality

--- internal/interfaces/grpc/media_handler.go
+++ internal/interfaces/grpc/media_handler.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"bytes"
 	"context"
+	"io"
 
 	pb "github.com/cinema-platform-organization/contracts/gen/go/media"
 	"github.com/cinema-platform-organization/media-service/internal/application/dto"
@@ -46,5 +47,46 @@ func (h *MediaHandler) Upload(
 
 	return &pb.UploadResponse{
 		Key: res.Key,
+	}, nil
+}
+
+func (h *MediaHandler) Delete(
+	ctx context.Context,
+	req *pb.DeleteRequest,
+) (*pb.DeleteResponse, error) {
+	res, err := h.deleteUC.Execute(ctx, dto.DeleteMediaRequest{
+		Key: req.Key,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.DeleteResponse{
+		Ok: res.OK,
+	}, nil
+}
+
+func (h *MediaHandler) Get(
+	ctx context.Context,
+	req *pb.GetRequest,
+) (*pb.GetResponse, error) {
+	res, err := h.getUC.Execute(ctx, dto.GetMediaRequest{
+		Key: req.Key,
+	})
+
+	if err != nil {
+		return nil, err
+	}
+	defer res.Reader.Close()
+
+	data, err := io.ReadAll(res.Reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.GetResponse{
+		Data:        data,
+		ContentType: res.ContentType,
 	}, nil
 }
